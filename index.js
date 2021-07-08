8da479735ef35e50af8251191993fca98eaa0333
Jenna Anderson <jennaanderson@Jennas-MacBook-Air.local>
2021-07-08 13:56:26 -0600

    created react app

--- index.js
+++ index.js
@@ -1,13 +1,30 @@
 require('dotenv').config()
+
+const cors = require('cors')
+const path = require('path')
 const express = require('express')
 
 const server = express()
 
+server.use(express.json())
+server.use(cors())
+server.use(express.static(path.join(__dirname, 'client/')))
+
 const PORT = process.env.PORT || 5000
+
+server.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client/public', 'index.html'))
+})
 
 server.get('/api', (req, res) => {
     res.json({
         message: "web43 is working and ready to serve"
+    })
+})
+
+server.use((req,res) => {
+    res.json({
+        message: "sorry could not serve"
     })
 })
 
